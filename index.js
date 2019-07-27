43e5244c729821e9aa943e8d814689772845fc43
rao123dk <dheerajkrao1993@gmail.com>
2019-07-27 16:17:10 +0530

    added some objects methods

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Website :- rao123dk.github.io
  */
 
-
+//Array
 Array.prototype.unique = function(){
 	return this.filter((value, index, self)=>(
 	   self.indexOf(value) === index
@@ -27,6 +27,29 @@ Array.prototype.clean = function(required){
 Array.prototype.copyArray = function(){
     return this.slice(0);
 }
+//String
+String.prototype.trim = function(){
+
+}
+
+//Number
+Number.prototype[Symbol.iterator] = function*(){
+	for(let i = 0; i<= this; i++){
+    	yield i;
+    }
+};
+
+//Object
+
+Object.prototype.has = function(data){
+    return !!data && data in this;
+}
+
+
+Object.prototype.empty = function(){
+    return !this && !Object.keys(this).length;
+}
+
 
 
 Array.prototype.sample = function(){
--- test/index.js
+++ test/index.js
@@ -13,3 +13,12 @@ console.log(names.clean('undefined'));
 
 var names = [8, null, undefined,0];
 console.log(names.copyArray());
+
+
+console.log([...3]);
+console.log(...10);
+
+let obj = { a:22, b:4}
+
+console.log(obj.has('a'))
+console.log(obj.has('c'))
